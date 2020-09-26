db65d1adf1c7ecc56363d34ebbc2bbf2b91e64ad
Manogya Acharya <acharyma@bc.edu>
2020-09-26 17:26:15 -0400

    5.6 completed

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Manogya Acharya on 9/26/20.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
